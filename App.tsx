00d13000dc6d5e61e8bbe0aecbc14dd8db44ef8b
asthwuisan <asthriedwuisan09@gmail.com>
2025-11-10 11:27:10 +0800

    testing splash screen & font family

--- App.tsx
+++ App.tsx
@@ -1,19 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { View } from 'react-native';
+import SplashScreen from './src/screens/SplashScreen';
 import OnboardingScreen from './src/screens/OnboardingScreen';
 
 const App = () => {
+  const [showSplash, setShowSplash] = useState(true);
+
+  useEffect(() => {
+    // Timer 2 detik sebelum pindah (bisa disesuaikan)
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <View
-      style={{
-        flex: 1,
-      }}
-    >
-      <OnboardingScreen />
+    <View style={{ flex: 1 }}>
+      {showSplash ? <SplashScreen /> : <OnboardingScreen />}
     </View>
   );
 };
 
 export default App;
-
-const styles = StyleSheet.create({});
